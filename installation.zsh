3973e0c5d943ace8dba8fcaca26992c34411bc0a
jakubgrad <113715885+jakubgrad@users.noreply.github.com>
2024-07-25 14:26:25 +0300

    Install vscode and download repository Bycycle after logging in

--- installation.zsh
+++ installation.zsh
@@ -1,21 +1,9 @@
 #!/bin/zsh
-
-echo "Creating Bycycle_all directory inside Documents"
-cd ~/Documents
-mkdir Bycycle_all
-
-echo "Entering Bycycle_all"
-cd Bycycle_all
-
-echo "After pressing enter, Xcode will open. In Xcode, you'll need to login with github account and a token and download the repository. Make sure to download the repository to ~/Documents/Bycycle_all. This script will run in the background for approximately 20 minutes without requiring any action."
-sleep 6
-open -a Xcode
+echo "You might want to install chrome, click on Download, then on googlechrome.dmg, and drag chrom to the taskbar (no to the Applications bc of admin priviledges)"
+open -a Safari https://www.google.com/chrome/
 
 echo "Ensuring ~/.zshenv exists"
 touch $HOME/.zshenv
-
-echo "Entering Bycycle_all"
-cd Bycycle_all
 
 echo "Downloading homebrew"
 git clone https://github.com/Homebrew/brew.git
@@ -29,14 +17,45 @@ source ~/.zshenv
 echo "Checking if brew works i.e. was added to the path correctly"
 brew --version
 
+echo "Installing Github CLI"
+brew install gh
+
+echo "Creating Bycycle_all directory inside Documents"
+cd ~/Documents
+mkdir Bycycle_all
+
+echo "Entering Bycycle_all"
+cd ~/Documents/Bycycle_all
+
+echo "Login to Github in the command line to download BasantaPoudel/Bycycle"
+gh auth login
+gh repo clone BasantaPoudel/Bycycle
+
+echo "Downloading vscode"
+curl -L --url "https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal" -o VSCode-darwin-universal.zip
+echo "Unzipping vscode"
+unzip VSCode-darwin-universal.zip
+echo "Removing the leftover .zip of vscode"
+rm -rf VSCode-darwin-universal.zip
+
+
 # Download Flutter SDK (ARM version)
 echo "Downloading Flutter SDK..."
 cd ~/Documents/Bycycle_all
 curl -O "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.22.2-stable.zip"
 
+# Extracting flutter zip
+echo "Setting Flutter SDK path..."
+unzip flutter_macos*
 echo "Setting Flutter SDK path..."
 echo 'export PATH="$HOME/Documents/Bycycle_all/flutter/bin:$PATH"' >> ~/.zshenv
 source ~/.zshenv
+
+echo "Installing flutter extension for VSCode"
+cd ~/Documents/Bycycle_all
+Visual\ Studio\ Code.app/Contents/Resources/app/bin/code --install-extension dart-code.flutter
+echo "Opening the downloaded Bycycle repository with VSCode"
+Visual\ Studio\ Code.app/Contents/Resources/app/bin/code ByCycle
 
 echo "Verifying Flutter installation..."
 flutter -v
@@ -46,9 +65,8 @@ echo "Installing cocoapds using brew. After 3-15 minutes a popup will ask for ad
 sleep 5
 brew install cocoapods
 
-
-echo "Installing VSCode. Highly prone to failure, maybe should be done by going to Apple Store instead"
-brew install --cask visual-studio-code
+echo "Installing command line neovim"
+brew install neovim
 
 echo "Setup probably completed successfully!"
 
@@ -60,3 +78,4 @@ sleep 2
 echo "flutter run inside ~/Documents/Bycycle_all/ByCycle"
 cd ~/Documents/Bycycle_all/ByCycle
 flutter run
+
